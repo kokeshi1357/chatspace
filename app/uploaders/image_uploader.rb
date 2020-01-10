4c7167c35a0b762b39125871662554476ccfbac5
kokeshi1357 <kokesi1357@icloud.com>
2020-01-10 11:24:54 +0900

    aws7

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -44,6 +44,7 @@ class ImageUploader < CarrierWave::Uploader::Base
   #   "something.jpg" if original_filename
   # end
   include CarrierWave::MiniMagick
-
   process resize_to_fit: [800, 800]
+
+  storage :fog
 end
